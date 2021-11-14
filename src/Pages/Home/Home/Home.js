e49a7b1e8457b84ad726d9b95759c0b9ceb653ff
MohammadRakib1e18 <trustedrakib@gmail.com>
2021-11-14 11:55:34 +0600

    added footer

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
 import Banner from '../Banner/Banner';
+import Footer from '../../Footer/Footer';
 
 const Home = () => {
     return (
         <div>
             <Navigation></Navigation>
             <Banner></Banner>
+            <Footer></Footer>
         </div>
     );
 };
